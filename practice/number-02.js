8c2d0b26e018aa396ed57d27c7fe7f871d9a4034
Hai Pham <haipham.ndev@gmail.com>
2022-02-02 15:38:34 +0700

    update number 02

--- practice/number-02.js
+++ practice/number-02.js
@@ -1,18 +1,29 @@
 // extract all digits and compare from right to left
 // convert to string and compare
+// export function isDecreasingNumber(n) {
+//   if (n < 10) return false;
+
+//   let remaining = n;
+//   let prevDigit = 0;
+
+//   while (remaining > 0) {
+//     const lastDigit = remaining % 10;
+//     if (lastDigit <= prevDigit) return false;
+
+//     prevDigit = lastDigit;
+
+//     remaining = Math.trunc(remaining / 10);
+//   }
+
+//   return true;
+// }
+
 export function isDecreasingNumber(n) {
   if (n < 10) return false;
 
-  let remaining = n;
-  let prevDigit = 0;
-
-  while (remaining > 0) {
-    const lastDigit = remaining % 10;
-    if (lastDigit <= prevDigit) return false;
-
-    prevDigit = lastDigit;
-
-    remaining = Math.trunc(remaining / 10);
+  const numberString = n.toString();
+  for (let i = 1; i < numberString.length; i++) {
+    if (numberString[i] >= numberString[i - 1]) return false;
   }
 
   return true;
